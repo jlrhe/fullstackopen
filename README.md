f524f97e6221d60c8826fb9337adddb91b496c14
Jan Heikel <jan@heikel.fi>
2019-07-20 22:02:32 +0300

    assignment 1.1 complete

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # fullstackopen
 assignments for the course in fullstackopen.com
+I'm doing the course in finnish, so some of the assignments (most notably the flow charts of part 0[osa0]) will be in finnish.
+In the code and comments I'll try to stick to using english, but a tired mind may sometimes default to finnish without me noticing.
+
+Since the course material omits semicolons I will try to do it also to keep consistency, but I might put a few in by habit (just browsed the flamewars that the conversations about this topic are - omitting them won simply because writing them on a finnish-layout keyboard is a hassle I never liked). 
--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,30 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const Header = (props) => {
+    return (
+        <h1>{props.courseName}</h1>
+    )
+}
 
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
+const Content = (props) => {
+    return (
+        //this should be a loop
+        <div>
+            <p>{props.partName1} {props.exerciseCount1}</p>
+            <p>{props.partName2} {props.exerciseCount2}</p>
+            <p>{props.partName3} {props.exerciseCount3}</p>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>Number of excersises {props.totalExercises}</p>
+    )
+}
+
+const App = () => {
+    //intended or not I'm constantly thinking half ass application development when I see this
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    return (
+        
+        <div>
+            <Header courseName={course}/>
+            {
+                //this is so ugly I'd puke if it wasn't temporary and getting fixed in the next part of the assignment
+            }
+            <Content partName1={part1} partName2={part2} partName3={part3} exerciseCount1={exercises1} exerciseCount2={exercises2} exerciseCount3={exercises3}/>
+            <Total totalExercises={exercises1 + exercises2 + exercises3}/>
+        </div>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
