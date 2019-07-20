27cc13710796188427fa7bcc4e177a13f99d4f33
Jan Heikel <jan@heikel.fi>
2019-07-20 22:42:44 +0300

    assignment 1.2 completed

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -11,21 +11,28 @@ const Content = (props) => {
     return (
         //this should be a loop
         <div>
-            <p>{props.partName1} {props.exerciseCount1}</p>
-            <p>{props.partName2} {props.exerciseCount2}</p>
-            <p>{props.partName3} {props.exerciseCount3}</p>
+            <Part partName={props.partName1} exerciseCount={props.exerciseCount1}/>
+            <Part partName={props.partName2} exerciseCount={props.exerciseCount2}/>
+            <Part partName={props.partName3} exerciseCount={props.exerciseCount3}/>
         </div>
     )
 }
 
 const Total = (props) => {
     return (
+        //not sure if I'm supposed to do the addition here or in the App as is, but I imagine it doesn't really matter at this point
         <p>Number of excersises {props.totalExercises}</p>
     )
 }
 
+const Part = (props) => {
+    return (
+        <p>{props.partName} {props.exerciseCount}</p>
+    )
+}
+
 const App = () => {
-    //intended or not I'm constantly thinking half ass application development when I see this
+    //intended or not I'm constantly thinking half ass application development when I see this. The course material says we'll improve on this so I won't bother doing anything about it at this time
     const course = 'Half Stack application development'
     const part1 = 'Fundamentals of React'
     const exercises1 = 10
@@ -39,7 +46,7 @@ const App = () => {
         <div>
             <Header courseName={course}/>
             {
-                //this is so ugly I'd puke if it wasn't temporary and getting fixed in the next part of the assignment
+                //not sure how much I'm allowed to improve over the material given, but I would really love to make some loops
             }
             <Content partName1={part1} partName2={part2} partName3={part3} exerciseCount1={exercises1} exerciseCount2={exercises2} exerciseCount3={exercises3}/>
             <Total totalExercises={exercises1 + exercises2 + exercises3}/>
